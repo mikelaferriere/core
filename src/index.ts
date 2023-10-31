300742fe56f4dddf60a1edbde444d1293b9dba86
Mike Laffy <nhlflash22@gmail.com>
2023-10-31 01:05:58 -0400

    add new packages

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,20 @@ export * as Tracing from './nodetracing'
 export * as Logging from './nodelogging'
 export * as Mqtt from './mqtt'
 export * as Request from './request'
+
+/** API */
+export * as Fitbit from './api/fitness/fitbit'
+export * as Withings from './api/fitness/withings'
+export * as Weather from './api/weather'
+
+/** Models */
+export * as TCalendar from './models/api/calendar'
+export * as TFitbit from './models/api/fitbit'
+export * as THue from './models//api/hue'
+export * as TNest from './models/api/nest'
+export * as TOAuth from './models/api/oauth2'
+export * as TSchedule from './models/api/schedule'
+export * as TSpeechRecognition from './models/api/speechRecognition'
+export * as TSports from './models/api/sports'
+export * as TWeather from './models/api/weather'
+export * as TWithings from './models/api/withings'
