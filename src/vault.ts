4e90dc3d4bfcbe822f361f45d3ff80f548993cc2
Mike Laffy <nhlflash22@gmail.com>
2023-11-11 12:50:17 -0500

    add hashicorp vault fetch token function

---
+++ src/vault.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const VAULT_HOST = "http://192.168.1.179:8200";
+
+export const fetchSecret = async (token: string, secretPath: string) => {
+  return axios.post(`${VAULT_HOST}/v1/cubbyhole${secretPath}`, {
+    headers: {
+      "X-Vault-Token": token,
+      "Content-Type": "application/json",
+      Accept: "*/*",
+    },
+  });
+};
