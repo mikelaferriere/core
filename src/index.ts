7a0e6deec78dd6f67dcd8b071f62de4aabe1207c
Mike Laffy <nhlflash22@gmail.com>
2023-11-14 03:54:45 +0000

    clean up exports

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,7 @@ export * as Tracing from './nodetracing'
 export * as Logging from './nodelogging'
 export * as Mqtt from './mqtt'
 export * as Request from './request'
+export * as Vault from './vault'
 
 /** API */
 export * as Api from './api'
-export * as Fitbit from './api/fitness/fitbit'
-export * as Withings from './api/fitness/withings'
-export * as Weather from './api/weather'
-
-/** Models */
-export * as Model from './models'
-export * from './models/api/calendar'
-export * from './models/api/fitbit'
-export * from './models//api/hue'
-export * from './models/api/nest'
-export * from './models/api/oauth2'
-export * from './models/api/schedule'
-export * from './models/api/speechRecognition'
-export * from './models/api/sports'
-export * from './models/api/weather'
-export * from './models/api/withings'
