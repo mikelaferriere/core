bba4419d75afb71420efc28688a79a85f18b6796
Mike Laffy <nhlflash22@gmail.com>
2023-11-19 20:01:21 +0000

    update signature of Summary.fetch

--- src/api/sports/summary.ts
+++ src/api/sports/summary.ts
@@ -1,1 +1,9 @@
-export const fetch = () => {}
+import { Enums } from "@mikelaferriere/espn-api";
+import { ScoringPlay } from "../../types/sports";
+
+export const fetch = (
+    league: Enums.League,
+    id: string
+): ScoringPlay[] => {
+    return []
+}
