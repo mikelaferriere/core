b9f0a2b3fd819057e67b011419e630a5c70fb32f
Mike Laffy <nhlflash22@gmail.com>
2023-12-12 15:16:13 +0000

    remove unecessary metadata

--- src/nodelogging.ts
+++ src/nodelogging.ts
@@ -35,9 +35,6 @@ const configure = ({
   return createLogger({
     level: String(minLogLevel),
     transports: [consoleTransport, lokiTransport],
-    defaultMeta: {
-      service: serviceName,
-    },
   })
 }
 
